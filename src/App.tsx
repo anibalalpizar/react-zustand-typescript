1d9e47a6229ec4327b54e055b2598525f773a28e
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-06 01:35:37 -0600

    Add post creation functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import "./App.css";
-import { useUserStore } from "./store";
+import { useState } from "react";
+import { type Post, usePostsStore, useUserStore } from "./store";
 
 function UpdateUserForm() {
   const { email, setUsername, setEmail, username } = useUserStore();
@@ -22,12 +22,52 @@ function UpdateUserForm() {
 
 function App() {
   const { email, username } = useUserStore();
+  const { posts, addPost } = usePostsStore();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   return (
     <>
       <h1>{username}</h1>
       <h2>{email}</h2>
       <UpdateUserForm />
+
+      <div>
+        <b>Create a new post</b>
+      </div>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+
+      <button
+        onClick={() => {
+          addPost({ id: Math.random().toString(), title, content });
+          setTitle("");
+          setContent("");
+        }}
+      >
+        Create post
+      </button>
+
+      <div>
+        <h1>Posts</h1>
+        {posts.map((post: Post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
