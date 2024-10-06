e047b8919c65b8826786570ff042a882a6e5cb23
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-05 22:51:44 -0600

    Add user store and update user form in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { useUserStore } from "./store";
+
+function UpdateUserForm() {
+  const { email, setUsername, setEmail, username } = useUserStore();
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+    </div>
+  );
+}
 
 function App() {
-  return <></>;
+  const { email, username } = useUserStore();
+
+  return (
+    <>
+      <h1>{username}</h1>
+      <h2>{email}</h2>
+      <UpdateUserForm />
+    </>
+  );
 }
 
 export default App;
