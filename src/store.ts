fa6e85ca46a02f18a37b7ba296ae0babaf4012e0
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-05 23:27:46 -0600

    Refactor user store to use Zustand devtools and create a user slice

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,22 @@
-import { create } from "zustand";
+import { create, type StateCreator } from "zustand";
+import { devtools } from "zustand/middleware";
 
-export interface UserStore {
+export interface UserSlice {
   username: string;
   email: string;
   setUsername: (username: string) => void;
   setEmail: (email: string) => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
+export const createUserSlice: StateCreator<UserSlice> = (set) => ({
   username: "",
   email: "",
   setUsername: (username: string) => set(() => ({ username })),
   setEmail: (email: string) => set(() => ({ email })),
-}));
+});
+
+export const useAppStore = create(
+  devtools<UserSlice>((...a) => ({
+    ...createUserSlice(...a),
+  }))
+);
